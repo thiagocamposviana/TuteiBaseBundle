141904ae8d044f436f2b89313fd2fb7e5bbf6242
Thiago Campos Viana <thiagocamposviana@gmail.com>
2013-12-21 15:41:34 -0200

    added dropdown search form

--- Resources/public/js/project.js
+++ Resources/public/js/project.js
@@ -1,5 +1,5 @@
 //Add Hover effect to menus
-$('ul.nav li.dropdown').hover(function() {
+$('ul.top-menu-items li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn();
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut();
