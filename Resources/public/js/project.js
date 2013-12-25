9c7a6d306a34d5a1855d44692dd5cf61780ed53b
Thiago Campos Viana <thiagocamposviana@gmail.com>
2013-12-25 15:53:56 -0200

    Created style switcher

--- Resources/public/js/project.js
+++ Resources/public/js/project.js
@@ -4,3 +4,18 @@ $('ul.top-menu-items li.dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut();
 });
+
+$("a[data-theme]").click(function() {
+    $("head link#theme").attr("href", $(this).data("theme"));
+});
+
+if($.cookie("css")) {
+	$("head link#theme").attr("href",$.cookie("css"));
+}
+$(document).ready(function() { 
+	$("a[data-theme]").click(function() {
+		$("head link#theme").attr("href", $(this).data("theme"));
+		$.cookie("css",$(this).data("theme"), {expires: 365, path: '/'});
+		return false;
+	});
+});
