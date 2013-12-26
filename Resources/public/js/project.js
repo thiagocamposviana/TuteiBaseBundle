d53a4718898c95a53fe6444cab2fdeb57f0a7d0d
Thiago Campos Viana <thiagocamposviana@gmail.com>
2013-12-25 22:56:06 -0200

    - Formated project.js code

--- Resources/public/js/project.js
+++ Resources/public/js/project.js
@@ -1,21 +1,18 @@
-//Add Hover effect to menus
-$('ul.top-menu-items li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn();
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut();
-});
-
-$("a[data-theme]").click(function() {
-    $("head link#theme").attr("href", $(this).data("theme"));
-});
-
 if($.cookie("css")) {
 	$("head link#theme").attr("href",$.cookie("css"));
 }
+
 $(document).ready(function() { 
-	$("a[data-theme]").click(function() {
-		$("head link#theme").attr("href", $(this).data("theme"));
-		$.cookie("css",$(this).data("theme"), {expires: 365, path: '/'});
-		return false;
-	});
+    //Add Hover effect to menus
+    $('ul.top-menu-items li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn();
+    }, function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut();
+    });
+    
+    $("a[data-theme]").click(function() {
+            $("head link#theme").attr("href", $(this).data("theme"));
+            $.cookie("css",$(this).data("theme"), {expires: 365, path: '/'});
+            return false;
+    });
 });
