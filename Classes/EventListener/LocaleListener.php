cae6831cd384a6a1174b0f6c0c7894543d5a6f8d
Thiago Campos Viana <thiagocamposviana@gmail.com>
2013-12-31 09:52:13 -0200

    - Added locale listener

---
+++ Classes/EventListener/LocaleListener.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * LocaleListener class
+ *
+ * @author Thiago Campos Viana <thiagocamposviana@gmail.com>
+ */
+
+
+namespace Tutei\BaseBundle\Classes\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class LocaleListener{
+
+    protected $container;
+   
+    public function setContainer($container){
+        $this->container = $container;        
+    }
+ 
+
+    public function onKernelRequest( GetResponseEvent $event )
+    {
+        $request = $event->getRequest();
+        $siteaccess = $this->container->get('ezpublish.siteaccess')->name;
+        $twigGlobals = $this->container->get('twig')->getGlobals();
+        $locale = $twigGlobals['siteaccess'][$siteaccess]['locale'];
+        $this->container->get('request')->setLocale($locale);        
+    }
+    
+    static public function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        );
+    }
+}
